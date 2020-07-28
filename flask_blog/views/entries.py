b0ce47908263bc43819fc261cf11dece5b835231
Kenta Nakamori <kenta@MacBook-Pro.local>
2020-07-29 01:37:29 +0900

    ediit required each

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -7,7 +7,7 @@ from flask_blog.models.entries import Entry, User
 def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    entries = Entry.query.filter_by(users_id=session['users_id']).order_by(Entry.id.desc()).all()
+    entries = Entry.query.order_by(Entry.id.desc()).all()
     return render_template('entries/index.html', entries=entries)
 
 
